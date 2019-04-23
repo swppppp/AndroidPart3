ab9c7242c68fe765c7e24dc1d48359cfe5102030
swppppp <sw931110@naver.com>
2019-04-23 14:30:36 +0900

    realm사용 java class

--- app/src/main/java/com/example/part3/RealmReadActivity.java
+++ app/src/main/java/com/example/part3/RealmReadActivity.java
@@ -1,7 +1,11 @@
 package com.example.part3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.realm.Realm;
 
 public class RealmReadActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class RealmReadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realm_read);
+
+        TextView titleView = findViewById(R.id.realm_read_title);
+        TextView contentView = findViewById(R.id.realm_read_content);
+
+        // intent에서 넘어온 문자열 추출
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+
+        Realm mRealm = Realm.getDefaultInstance();
+        MemoVO vo = mRealm.where(MemoVO.class).equalTo("title", title).findFirst();
+        titleView.setText(vo.title);
+        contentView.setText(vo.content);
     }
 }
